c509f35a0355e817b673588d0cc679e58cb33f8c
Carlos Sevilla <sevillecarlos@gmail.com>
2021-10-16 23:52:37 -0600

    add style to rules modal

--- src/components/ModalRules.tsx
+++ src/components/ModalRules.tsx
@@ -1,6 +1,9 @@
 import React from "react";
 //import bootstrap
-import { Modal } from "react-bootstrap";
+import { Modal, Table } from "react-bootstrap";
+//style
+import "./style/ModalRules.css";
+
 //interface for the props
 interface ModalRulesI {
   showModal: boolean;
@@ -10,12 +13,64 @@ interface ModalRulesI {
 const ModalRules = (props: ModalRulesI) => {
   const { showModal, handleCloseModal } = props;
   return (
-    <div>
-      <Modal show={showModal} onHide={handleCloseModal}>
-        <Modal.Header closeButton>
-          <Modal.Title>Modal heading</Modal.Title>
+    <div className="rules">
+      <Modal
+        backdropClassName="back-drop-modal"
+        contentClassName="rules-modal"
+        show={showModal}
+        onHide={handleCloseModal}
+      >
+        <Modal.Header className="rules-heading" closeButton>
+          <Modal.Title className="rules-heading-title">
+            Rewards Rules
+          </Modal.Title>
         </Modal.Header>
-        <Modal.Body>Woohoo, you're reading this text in a modal!</Modal.Body>
+        <Modal.Body className="rules-body">
+          <Table className='rules-table' borderless >
+            <tbody>
+              <tr>
+                <td>50</td>
+
+                <td>🍒</td>
+                <td>🍒</td>
+                <td>🍒</td>
+              </tr>
+              <tr>
+                <td>40</td>
+                <td>🍒</td>
+                <td>🍒</td>
+              </tr>
+              <tr>
+                <td>20</td>
+                <td>🍎</td>
+                <td>🍎</td>
+                <td>🍎</td>
+              </tr>
+              <tr>
+                <td>10</td>
+                <td>🍎</td>
+                <td>🍎</td>
+              </tr>
+              <tr>
+                <td>15</td>
+                <td>🍌</td>
+                <td>🍌</td>
+                <td>🍌</td>
+              </tr>
+              <tr>
+                <td>5</td>
+                <td>🍌</td>
+                <td>🍌</td>
+              </tr>
+              <tr>
+                <td>3</td>
+                <td>🍋</td>
+                <td>🍋</td>
+                <td>🍋</td>
+              </tr>
+            </tbody>
+          </Table>
+        </Modal.Body>
       </Modal>
     </div>
   );
